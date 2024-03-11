d23a0ef4f7b6d4a0c566ac31af8f6b2092a678f4
Brandon Chung <chung.brandon123@gmail.com>
2024-03-11 02:18:17 -0500

    Add google vertex ai

---
+++ internal/env/load.go
@@ -0,0 +1,31 @@
+package env
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnvVariables() error {
+	if err := godotenv.Load("../../.env"); err != nil {
+		return fmt.Errorf("unable to load .env file")
+	}
+
+	envKeys := []string{
+		"SUPABASE_URL",
+		"SUPABASE_KEY",
+		"GOOGLE_APPLICATION_CREDENTIALS",
+		"GOOGLE_CLOUD_PROJECT_ID",
+		"GOOGLE_CLOUD_REGION",
+		"GOOGLE_CLOUD_VERTEX_MODEL_NAME",
+	}
+
+	for _, key := range envKeys {
+		if os.Getenv(key) == "" {
+			return fmt.Errorf("missing env variable key or value %s", key)
+		}
+	}
+
+	return nil
+}
