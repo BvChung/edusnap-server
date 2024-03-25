5e5fa97cc1657740463f3416c5c2748038b70f67
Brandon Chung <chung.brandon123@gmail.com>
2024-03-25 04:24:04 -0500

    Add tests for missing environment variables in LoadEnvVariables function

--- internal/env/load_test.go
+++ internal/env/load_test.go
@@ -5,6 +5,25 @@ import (
 )
 
 func Test_LoadEnv(t *testing.T) {
+	envKeys := []string{
+		"SUPABASE_URL",
+		"SUPABASE_KEY",
+		"GOOGLE_APPLICATION_CREDENTIALS",
+		"GOOGLE_CLOUD_PROJECT_ID",
+		"GOOGLE_CLOUD_REGION",
+		"GOOGLE_CLOUD_VERTEX_MODEL_NAME",
+	}
+
+	for _, key := range envKeys{
+		t.Run("Test missing env variables error", func(t *testing.T) {
+			t.Setenv(key, "")
+
+			if err := LoadEnvVariables("../../.env"); err != nil {
+				t.Fatal(err.Error())
+			}
+		})
+	}
+
 	t.Run("Test loading env variables", func(t *testing.T) {
 		if err := LoadEnvVariables("../../.env"); err != nil {
 			t.Fatal(err.Error())
