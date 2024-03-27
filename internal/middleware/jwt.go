040472394d1a2d0849c1a15a43e8c2ad1095b24c
Brandon Chung <chung.brandon123@gmail.com>
2024-03-27 05:33:49 -0500

    Add JWT middleware for authentication

---
+++ internal/middleware/jwt.go
@@ -0,0 +1,27 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/ocr/internal/response"
+)
+
+func JWTHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		tokenStr := strings.Split(authHeader, " ")[1]
+
+		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+			return []byte("cryptokey"), nil
+		})
+
+		if err != nil || !token.Valid {
+			response.NewErrorResponse(w, "Invalid JWT token", response.InvalidRequest, http.StatusBadRequest)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
