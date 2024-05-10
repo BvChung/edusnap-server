7cdd78dddd97d817d4106f8dfb11a6cc6187c74a
Brandon Chung <chung.brandon123@gmail.com>
2024-05-09 21:35:07 -0500

    Update jwt auth

--- cmd/middleware/jwt.go
+++ cmd/middleware/jwt.go
@@ -1,27 +1,69 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/ocr/cmd/response"
 )
 
-func JWTHandler(next http.Handler) http.Handler {
+var secretKey = []byte("WWTm00/88EzBgk5y7IlcpZDIDDUP2vtA/EuCDmujo5j2pD0ZjRll/TaLlYoE4KlQUg7LsSMUFAU278nSrF/Jhg==")
+
+func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
-		tokenStr := strings.Split(authHeader, " ")[1]
 
-		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-			return []byte("cryptokey"), nil
-		})
-
-		if err != nil || !token.Valid {
-			response.NewErrorResponse(w, "Invalid JWT token", response.InvalidRequest, http.StatusBadRequest)
+		if authHeader == "" {
+			w.WriteHeader(http.StatusForbidden)
+			fmt.Fprintf(w, "Unauthorized no authorization header")
 			return
 		}
 
+		jwtToken := strings.Split(authHeader, " ")[1]
+
+		_, err := validateToken(jwtToken)
+		if err != nil {
+			w.WriteHeader(http.StatusForbidden)
+			fmt.Fprintf(w, "invalid jwt token")
+			return
+		}
+
+		// fmt.Println(t.Claims)
+		// fmt.Println(t.Claims.GetIssuer())
+
 		next.ServeHTTP(w, r)
 	})
+}
+
+func createToken() (string, error) {
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"iss": "ocr-app",                        // Issuer
+		"exp": time.Now().Add(time.Hour).Unix(), // Expiration time
+		"iat": time.Now().Unix(),                // Issued at
+	})
+
+	tokenStr, err := claims.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenStr, nil
+}
+
+func validateToken(token string) (*jwt.Token, error) {
+	t, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !t.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	return t, nil
 }
